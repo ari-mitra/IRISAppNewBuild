cbc9f3b9c53522de19c7957203959c75d68f775e
arindam <ari_mitra@yahoo.com>
2015-06-18 17:32:59 +0530

    initial commit

--- js/apicall.js
+++ js/apicall.js
@@ -1,9 +1,17 @@
-//var BASE_URL = "http://localhost/iris/";
-var BASE_URL = "http://dev.wrctechnologies.com/irisdesign/dev/";
+var BASE_URL = "http://localhost/iris/dev/";
+//var BASE_URL = "http://dev.wrctechnologies.com/irisdesign/dev/";
+
+// Regular Expression for Email.
+var regex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
 $(window).load(function () {
     $("#formSubmitReg1").click(function () {
         if (validateForm()) {
-            callApiForRegistration();
+            var todo = "reg";
+            var fname = $('#fname').val();
+            var lname = $('#lname').val();
+            var email = $('#email').val();
+            var password = $('#password').val();
+            callApiForRegistration(todo, fname, lname, email, password);
         }
     });
 });
@@ -12,63 +20,130 @@ $(window).load(function () {
 function validateForm() {
     var bool = true;
     if ($.trim($("#fname").val()) === "" || $.trim($("#fname").val().length) === 0) {
-//        alert("fname");
         $("#fname").addClass("error");
         $("#fname").focus();
-//        navigator.notification.alert("Firstname should not be left blank.", null, "Notification", "OK");
         bool = false;
     } else if ($.trim($("#lname").val()) === "" || $.trim($("#lname").val().length) === 0) {
-//        alert("lname");
         $("#lname").addClass("error");
         $("#lname").focus();
-//        navigator.notification.alert("Lastname should not be left blank.", null, "Notification", "OK");
         bool = false;
     } else if ($.trim($("#email").val()) === "" || $.trim($("#email").val().length) === 0) {
-//        alert("email");
         $("#email").addClass("error");
         $("#email").focus();
-//        navigator.notification.alert("Email should not be left blank.", null, "Notification", "OK");
         bool = false;
-    } else if ($.trim($("#username").val()) === "" || $.trim($("#username").val().length) === 0) {
-//        alert("uname");
-        $("#username").addClass("error");
-        $("#username").focus();
-//        navigator.notification.alert("Username should not be left blank.", null, "Notification", "OK");
+    } else if (!regex.test($("#email").val())) {
+        $("#email").addClass("error");
+        $("#email").focus();
+        bool = false;
+    } else if (!checkUniqueEmail($("#email").val())) {
+        $("#email").addClass("error");
+        $("#email").focus();
+        navigator.notification.alert("Your email id must be unique", null, "Notification", "OK");
         bool = false;
     } else if ($.trim($("#password").val()) === "" || $.trim($("#password").val().length) === 0) {
-//        alert("pwd");
         $("#password").addClass("error");
         $("#password").focus();
-//        navigator.notification.alert("Password should not be left blank.", null, "Notification", "OK");
+        bool = false;
+    } else if ($.trim($("#cpassword").val()) === "" || $.trim($("#cpassword").val().length) === 0) {
+        $("#cpassword").addClass("error");
+        $("#cpassword").focus();
+        bool = false;
+    } else if ($.trim($("#cpassword").val()) !== $.trim($("#password").val())) {
+        $("#cpassword").addClass("error");
+        $("#cpassword").focus();
         bool = false;
     } else {
         bool = true;
     }
     return bool;
 }
-function callApiForRegistration() {
-    var fname = $('#fname').val();
-    var lname = $('#lname').val();
-    var email = $('#email').val();
-    var username = $('#username').val();
-    var password = $('#password').val();
+function callApiForRegistration(todo, fname, lname, email, password) {
     $.ajax({
         url: BASE_URL + 'api/registration',
         type: "POST",
         data: {
+            todo: todo,
             fname: fname,
             lname: lname,
             email: email,
-            username: username,
             password: password
         },
         success: function (resp) {
             var data = $.parseJSON(resp);
             if (data.code == '200') {
-                location.href = 'selectinterest.html';
                 sessionStorage.setItem("uid", data.uid);
+                location.href = 'selectinterest.html';
+                if (data.toast !== "") {
+                    navigator.notification.alert(data.toast, null, "Notification", "OK");
+                }
             } else {
                 navigator.notification.alert(data.error, null, "Notification", "OK");
+            }
+        }
+    });
+}
+function getBankDetails() {
+    $.ajax({
+        url: BASE_URL + 'api/getSignUpBank',
+        type: 'POST',
+        data: $("#loginForm").serialize(),
+        success: function (resp) {
+            var obj = $.parseJSON(resp);
+            if (obj.code == '200') {
+                $("#signInBankName").text("SIGN IN WITH " + obj.bankname);
+            } else {
+                $("#signInBankName").text("SIGN IN WITH BANK OF AMERICA");
+            }
+        }
+    });
+}
+function regEmailFunction() {
+    onLoadRegEmailFunction();
+    $("#formSubmitRegEmail").click(function () {
+        if (validateRegWithEmailForm()) {
+//            alert();
+            var todo = "regwithbank";
+            var fname = "Test";
+            var lname = "Test";
+            var email = $('#email').val();
+            var username = "test";
+            var password = $('#password').val();
+            callApiForRegistration(todo, fname, lname, email, username, password);
+        }
+    });
+}
+function validateRegWithEmailForm() {
+    var bool = true;
+
+    if ($.trim($("#email").val()) === "" || $.trim($("#email").val().length) === 0) {
+        $("#email").addClass("error");
+        $("#email").focus();
+        bool = false;
+    } else if (!regex.test($("#email").val())) {
+        $("#email").addClass("error");
+        $("#email").focus();
+        bool = false;
+    } else if (!checkUniqueEmail($("#email").val())) {
+        $("#email").addClass("error");
+        $("#email").focus();
+        navigator.notification.alert("Your email id must be unique", null, "Notification", "OK");
+        bool = false;
+    } else {
+        bool = true;
+    }
+    return bool;
+}
+function onLoadRegEmailFunction() {
+    $.ajax({
+        url: BASE_URL + 'api/getSignUpBankLogo',
+        type: 'POST',
+        data: $("#loginForm").serialize(),
+        success: function (resp) {
+            var obj = $.parseJSON(resp);
+            if (obj.code == '200') {
+                $("#bankimage").html('<img src="' + obj.banklogo + '" draggable="false" height="130" height="129"/>');
+            } else {
+                $("#signInBankName").text("SIGN IN WITH BANK OF AMERICA");
             }
         }
     });
@@ -133,48 +208,212 @@ function addClassInterests(id) {
 
 // LOGIN CODE
 function loginFunctions() {
+    getBankDetails();
     $('.logsin').click(function () {
-        var username = $('#username').val();
-        var password = $('#password').val();
-        if (username == "") {
-            $('#username').addClass('error');
-            $('#username').focus();
-        } else if (password == "") {
-            $('#password').addClass('error');
-            $('#password').focus();
-        } else {
-            $.ajax({
-                url: BASE_URL + 'api',
-                data: {'username': username, 'password': password},
-                type: 'post',
-                success: function (resp) {
-                    var obj = $.parseJSON(resp);
-                    if (obj.code == '100') {
-                        sessionStorage.setItem("uid", obj.uid);
-                        location.href = 'dashboard.html';
-                    } else {
-                        //alert(obj.msg);
-                        $('#username').addClass('error');
-                        $('#username').focus();
-                    }
-                }
-            });
+        if (validateLoginForm()) {
+            fetchLoginData();
         }
     });
 }
+function validateLoginForm() {
+    var bool = true;
+    if ($.trim($("#email").val()) === "" || $.trim($("#email").val().length) === 0) {
+        $("#email").addClass("error");
+        $("#email").focus();
+        bool = false;
+    } else if (!regex.test($("#email").val())) {
+        $("#email").addClass("error");
+        $("#email").focus();
+        bool = false;
+    } else if ($.trim($("#password").val()) === "" || $.trim($("#password").val().length) === 0) {
+        $("#password").addClass("error");
+        $("#password").focus();
+        bool = false;
+    } else {
+        bool = true;
+    }
+    return bool;
+}
+function fetchLoginData() {
+    $.ajax({
+        url: BASE_URL + 'api',
+        type: 'POST',
+        data: $("#loginForm").serialize(),
+        success: function (resp) {
+            var obj = $.parseJSON(resp);
+            if (obj.code == '100') {
+                sessionStorage.setItem("uid", obj.uid);
+                location.href = 'dashboard.html';
+            } else {
+                navigator.notification.alert("Sorry wrong username or password. Please try again.", null, "Notification", "OK");
+                $('#username').addClass('error');
+                $('#username').focus();
+            }
+        }
+    });
+}
+
 // LOGIN CODE
 
 // DASHBOARD FUNCTIONS
 function dashboardFunctions() {
+    checkUserSession();
+}
+// DASHBOARD FUNCTIONS
+
+// GROUP FUNCTONS
+function groupFunction() {
+    checkUserSession();
+    fetchPublicGroups();
+    fetchPrivateGroups();
+}
+function createGroup() {
+    if (validateGroup()) {
+        $("#uid").val(sessionStorage.getItem("uid"));
+        $.ajax({
+            url: BASE_URL + 'api/createGroup',
+            type: 'POST',
+            data: $("#createGroupForm").serialize(),
+            success: function (resp) {
+                var data = $.parseJSON(resp);
+//                navigator.notification.alert(data.toast, null, "Notification", "OK");
+//                location.href = "publicgroup.html";
+                window.location.reload();
+            }
+        });
+    }
+}
+function validateGroup() {
+    var bool = true;
+    if ($.trim($("#group_name").val()) === "" || $.trim($("#group_name").val().length) === 0) {
+        $("#group_name").addClass("error");
+        $("#group_name").focus();
+        bool = false;
+    } else if ($.trim($("#group_content").val()) === "" || $.trim($("#group_content").val().length) === 0) {
+        $("#group_content").addClass("error");
+        $("#group_content").focus();
+        bool = false;
+    } else {
+        bool = true;
+    }
+    return bool;
+}
+function fetchPublicGroups() {
+    $.ajax({
+        url: BASE_URL + 'api/fetchUsersPublicGroups',
+        type: 'POST',
+        data: "uid=" + sessionStorage.getItem("uid"),
+        success: function (resp) {
+            console.log(resp);
+            var data = $.parseJSON(resp);
+            var groupListAppend = "";
+            for (var i = 0; i < data.length; i++) {
+                groupListAppend += constructGroupList(data[i].group_name, data[i].group_content);
+            }
+            $("#groupsDisplay").append(groupListAppend);
+        }
+    });
+}
+function fetchPrivateGroups() {
+    $.ajax({
+        url: BASE_URL + 'api/fetchUsersPrivateGroups',
+        type: 'POST',
+        data: "uid=" + sessionStorage.getItem("uid"),
+        success: function (resp) {
+            console.log(resp);
+            var data = $.parseJSON(resp);
+            var groupListAppend = "";
+            for (var i = 0; i < data.length; i++) {
+                groupListAppend += constructGroupList(data[i].group_name, data[i].group_content);
+            }
+            $("#groupsDisplayPrivate").append(groupListAppend);
+        }
+    });
+}
+function constructGroupList(groupName, groupContent) {
+    var div = '<div class="col-md-4">' +
+            '<article>' +
+            '<header>'+groupName+'</header>' +
+            '<div class="blog-image">' +
+            '<img alt="" src="img/img3.jpg" class="img-responsive">' +
+            '<div class="row text-center groupbtn">' +
+            '<div class="col-sm-4"><a href="#" class="groupsbtn">FOLLOW</a></div>' +
+            '<div class="col-sm-4"><a href="#" class="groupsbtn">JOIN</a></div>' +
+            '<div class="col-sm-4"><a href="#" class="groupsbtn">223</a></div>' +
+            '</div>' +
+            '</div>' +
+            '<div class="card-body blog-text">' +
+            '<p>'+groupContent+'</p>' +
+            '</div>' +
+            '</article>' +
+            '</div>';
+    return div;
+}
+// GROUP FUNCTONS
+// COMMON FUNCTIONS
+function checkUserSession() {
     if (null != sessionStorage.getItem("uid")) {
         sessionStorage.setItem("login", "true");
     } else {
         location.href = "index.html";
     }
 }
-// DASHBOARD FUNCTIONS
-
 function logout() {
     sessionStorage.clear();
     location.href = "index.html";
+}
+
+function checkUniqueEmail(email) {
+    $.ajax({
+        url: BASE_URL + 'api/checkUniqueEmail',
+        type: 'POST',
+        data: {email: email},
+        success: function (resp) {
+//            console.log(resp);
+            if (resp == 0) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+    });
+}
+
+// IMAGE UPLOAD ON IOS
+function getImage() {
+    // Retrieve image file location from specified source
+    navigator.camera.getPicture(uploadPhoto, function (message) {
+        alert('get picture failed');
+    }, {
+        quality: 50,
+        destinationType: navigator.camera.DestinationType.FILE_URI,
+        sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY
+    }
+    );
+
+}
+function uploadPhoto(imageURI) {
+    var options = new FileUploadOptions();
+    options.fileKey = "file";
+    options.fileName = imageURI.substr(imageURI.lastIndexOf('/') + 1);
+    options.mimeType = "image/jpeg";
+
+    var params = new Object();
+    params.value1 = "test";
+    params.value2 = "param";
+
+    options.params = params;
+    options.chunkedMode = false;
+
+    var ft = new FileTransfer();
+    ft.upload(imageURI, "http://yourdomain.com/upload.php", win, fail, options);
+}
+function win(r) {
+    console.log("Code = " + r.responseCode);
+    console.log("Response = " + r.response);
+    console.log("Sent = " + r.bytesSent);
+    alert(r.response);
+}
+function fail(error) {
+    alert("An error has occurred: Code = " + error.code);
 }
